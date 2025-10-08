23c0e2e8a899f1522d679c69658a8ea33ae94e63
DAHSS ILYAS <2006091400104@ofppt-edu.ma>
2025-10-08 17:48:41 +0100

    this is a new commit router

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -1,9 +1,8 @@
 function App() {
   return (
-    <div>
-      <h1 >Hello World</h1>
-      <p>This is my first React app!</p>
-    </div>
+    <>
+
+    </>
   );
 }
 
--- myreact/src/index.js
+++ myreact/src/index.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home";
+import HTML from "./pages/html";
+import Css from "./pages/css";
+import Javascript from "./pages/javascript";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <h1 style={{ backgroundColor: "red", textAlign: "center", fontSize: "24px" , color: "white" , padding: "20px", marginTop: "20px"}}>
+  SORRY.........
+</h1>
+
+  },
+
+  {
+    path: "/html",
+    element: <HTML />,
+  },
+
+  {
+    path: "/css",
+    element: <Css />,
+  },
+  {
+    path: "/javascript",
+    element: <Javascript />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
